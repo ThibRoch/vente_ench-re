96c15c3a5b6296b282ec9dad283ff2db07565098
ThibRoch <121017868+ThibRoch@users.noreply.github.com>
2023-01-02 16:21:35 +0100

    Update ServletConnection.java

--- ServletConnection.java
+++ ServletConnection.java
@@ -25,7 +25,7 @@ public class ServletConnection extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/seConnecter.jsp");
+		RequestDispatcher rd = request.getRequestDispatcher("/seConnecter.jsp");
 		rd.forward(request, response);
 	}
 
